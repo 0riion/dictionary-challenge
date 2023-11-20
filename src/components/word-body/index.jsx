849283d0897ab962a6236665bbf0bed4cbdfcf27
Julio Flores <juliocesarflores12@gmail.com>
2023-11-20 14:26:47 -0400

    feat: full word body

--- src/components/word-body/index.jsx
+++ src/components/word-body/index.jsx
@@ -3,39 +3,56 @@ import React from 'react';
 const WordBody = ({ word, data }) => {
   return (
     <div className="grid grid-cols-1 gap-4 pt-10">
-      <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white uppercase">
+      <h1 className="text-slate-900 font-extrabold text-5xl tracking-tight text-center dark:text-white capitalize">
         {word}
       </h1>
 
-      {data?.map((variant, index) => (
-        <div key={index}>
-          {variant.meanings?.map((meaning) => (
-            <div key={meaning.partOfSpeech}>
-              <div className="flex justify-between">
-                <h2 className="text-xl font-bold">
-                  <span className="capitalize">{word}</span>
-                  <span className="uppercase ml-2">({meaning.partOfSpeech})</span>
-                </h2>
+      {data?.map((variant, index) => {
+        const { meanings, phonetic, phonetics, sourceUrls, word } = variant;
+
+        return (
+          <>
+            {index === 0 && (
+              <>
+                <div key={phonetic + index} className="flex justify-between">
+                  <h2 className="text-xl">
+                    <a href={sourceUrls} target='_blank'>
+                      <span className="capitalize font-bold underline ml-2">
+                        {word}
+                      </span>
+                    </a>
+                    {phonetic}
+                  </h2>
+                  <div>
+                    <button className="btn btn-circle">UK</button>
+                    <button className="btn btn-circle">USA</button>
+                  </div>
+                </div>
 
                 <div>
-                  <button className="btn btn-circle">UK</button>
-                  <button className="btn btn-circle">USA</button>
+                  <p>
+                    <span className='font-bold'>Principal definition: </span>
+                    {meanings[0]['definitions'][0]['definition']}
+                  </p>
                 </div>
-              </div>
+              </>
+            )}
 
-              <ul>
-                {meaning.definitions.map((definition) => (
-                  <li key={definition.definition}>
-                    <p>- {definition.definition}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-
-          <div className="divider" />
-        </div>
-      ))}
+            {meanings.map((meaning) => {
+              const { partOfSpeech, definitions } = meaning;
+              console.log(meaning);
+              return (
+                <>
+                  <div className="divider">{partOfSpeech}</div>
+                  {definitions.map(({ definition }) => {
+                    return <p>- {definition}</p>
+                  })}
+                </>
+              );
+            })}
+          </>
+        );
+      })}
     </div>
   );
 };
