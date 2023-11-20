af6fd1401df85c03336b3064136476be91842d77
Julio Flores <juliocesarflores12@gmail.com>
2023-11-20 15:50:39 -0400

    feat: word pronunciation

--- src/components/word-body/index.jsx
+++ src/components/word-body/index.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Howl } from 'howler';
 
 const WordBody = ({ word, data }) => {
+
+  function getLocale(url) {
+    const path = new URL(url).pathname;
+    const locale = path.split("-").slice(-1)[0].split(".")[0];
+    return locale;
+  }
+
+  const handleAudio = (audioPath) => {
+    let sound = new Howl({
+      src: [audioPath]
+    });
+
+    sound.play();
+  };
+
   return (
     <div className="grid grid-cols-1 gap-4 pt-10">
       <h1 className="text-slate-900 font-extrabold text-5xl tracking-tight text-center dark:text-white capitalize">
@@ -9,14 +25,13 @@ const WordBody = ({ word, data }) => {
 
       {data?.map((variant, index) => {
         const { meanings, phonetic, phonetics, sourceUrls, word } = variant;
-
         return (
-          <>
+          <div key={index + word}>
             {index === 0 && (
               <>
                 <div key={phonetic + index} className="flex justify-between">
                   <h2 className="text-xl">
-                    <a href={sourceUrls} target='_blank'>
+                    <a href={sourceUrls} target="_blank" without rel="noreferrer">
                       <span className="capitalize font-bold underline ml-2">
                         {word}
                       </span>
@@ -24,8 +39,14 @@ const WordBody = ({ word, data }) => {
                     {phonetic}
                   </h2>
                   <div>
-                    <button className="btn btn-circle">UK</button>
-                    <button className="btn btn-circle">USA</button>
+                    {phonetics.map(({ audio }) => {
+                      const locale = getLocale(audio);
+                      return (
+                        <button className="btn btn-xs btn-secondary btn-circle mx-1" onClick={() => handleAudio(audio)} key={audio}>
+                          {locale}
+                        </button>
+                      );
+                    })}
                   </div>
                 </div>
 
@@ -40,7 +61,6 @@ const WordBody = ({ word, data }) => {
 
             {meanings.map((meaning) => {
               const { partOfSpeech, definitions } = meaning;
-              console.log(meaning);
               return (
                 <>
                   <div className="divider">{partOfSpeech}</div>
@@ -50,7 +70,7 @@ const WordBody = ({ word, data }) => {
                 </>
               );
             })}
-          </>
+          </div>
         );
       })}
     </div>
