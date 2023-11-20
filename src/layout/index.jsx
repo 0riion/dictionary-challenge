096d7abbb1bd00688238af2ea7add6607240c971
Julio Flores <juliocesarflores12@gmail.com>
2023-11-20 09:53:44 -0400

    feat: app layout

---
+++ src/layout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Layout({ children }) {
+
+  return (
+    <main className="max-w-5xl mx-auto h-screen py-10 px-5">
+      {children}
+    </main>
+  )
+
+};
+
+export default Layout;
